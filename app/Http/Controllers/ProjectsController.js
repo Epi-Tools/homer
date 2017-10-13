fb360cded191dc008ee3e4370a6c624e8b522410
Yann Carlen <yann-carlen@hotmail.fr>
2017-10-13 17:16:42 +0200

    add validation form projects

--- app/Http/Controllers/ProjectsController.js
+++ app/Http/Controllers/ProjectsController.js
@@ -1,6 +1,7 @@
 'use strict'
 
 const Projects = use('App/Model/Project')
+const Validator = use('Validator')
 
 class ProjectsController {
 
@@ -14,7 +15,33 @@ class ProjectsController {
     }
 
     * store (request, response) {
-
+        const only = [ 'name', 'description', 'github', 'presentation',
+            'spices', 'followUp1', 'followUp2', 'delivery', 'members',
+            'dateFollowUp1', 'dateFollowUp2', 'dateDelivery' ]
+        const data = request.only(...only)
+        const rules = {
+            name: 'required|max:255',
+            description: 'required',
+            github: 'url',
+            presentation: 'required|url',
+            spices: 'required',
+            followUp1: 'required',
+            followUp2: 'required',
+            delivery: 'required',
+            members: 'required',
+            dateFollowUp1: 'required|date',
+            dateFollowUp2: 'required|date',
+            dateDelivery: 'required|date'
+        }
+        const validation = yield Validator.validate(data, rules) 
+        if (validation.fails()) {
+            yield request.withOnly(...only).andWith({ errors: validation.messages() })
+            .flash() 
+            response.redirect('back')
+            return
+        }
+        yield Post.create(postData) 
+        response.redirect('/projects')
     }
 
 }
