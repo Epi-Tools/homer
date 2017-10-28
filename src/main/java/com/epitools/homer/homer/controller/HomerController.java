6eca7a20f29f324b7cfbf76db64e47d8e7c4d581
Yann Carlen <yann-carlen@hotmail.fr>
2017-10-29 00:53:46 +0200

    templates

---
+++ src/main/java/com/epitools/homer/homer/controller/HomerController.java
@@ -0,0 +1,44 @@
+package com.epitools.homer.homer.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomerController {
+
+    @GetMapping("/")
+    public String defaultPage() {
+        return "home";
+    }
+
+    @GetMapping("/home")
+    public String home() {
+        return "home";
+    }
+
+    @GetMapping("/admin")
+    public String admin() {
+        return "admin";
+    }
+
+    @GetMapping("/user")
+    public String user() {
+        return "user";
+    }
+
+    @GetMapping("/about")
+    public String about() {
+        return "about";
+    }
+
+    @GetMapping("/login")
+    public String login() {
+        return "login";
+    }
+
+    @GetMapping("/403")
+    public String error403() {
+        return "error/403";
+    }
+
+}
