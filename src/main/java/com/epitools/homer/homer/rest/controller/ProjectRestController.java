4bebc44dd8ab557cdea7c6c750b6925b1e972e55
Yann Carlen <yann-carlen@hotmail.fr>
2017-10-31 22:23:01 +0100

    Add annotations on Project REST Controller

--- src/main/java/com/epitools/homer/homer/rest/controller/ProjectRestController.java
+++ src/main/java/com/epitools/homer/homer/rest/controller/ProjectRestController.java
@@ -5,14 +5,13 @@ import com.epitools.homer.homer.repository.ProjectRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
 import java.util.List;
 
+@RestController
+@RequestMapping("/api")
 public class ProjectRestController {
     
     @Autowired
