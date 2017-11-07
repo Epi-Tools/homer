c06b24f00e3b2bca03f6c4a4fcefed1ca769a4ad
Yann Carlen <yann-carlen@hotmail.fr>
2017-11-07 18:04:11 +0100

    blih auth and register

--- src/main/java/com/epitools/homer/homer/provider/CustomAuthenticationProvider.java
+++ src/main/java/com/epitools/homer/homer/provider/CustomAuthenticationProvider.java
@@ -2,6 +2,9 @@ package com.epitools.homer.homer.provider;
 
 import com.epitools.homer.homer.model.User;
 import com.epitools.homer.homer.repository.UserRepository;
+import com.epitools.homer.homer.util.Utils;
+import org.json.JSONException;
+import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -12,6 +15,10 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+import java.security.InvalidKeyException;
+import java.security.NoSuchAlgorithmException;
+import java.security.SignatureException;
 import java.util.HashSet;
 import java.util.Set;
 
@@ -24,15 +31,38 @@ public class CustomAuthenticationProvider implements AuthenticationProvider {
     @Autowired
     private PasswordEncoder passwordEncoder;
 
+    // TODO(carlendev) fix when user doesn't exist
     @Override
     public Authentication authenticate(Authentication authentication) throws AuthenticationException {
-        final String email= authentication.getName();
+        final String email = authentication.getName();
         final String password = authentication.getCredentials().toString();
-        final User user = userRepository.findByEmail(email);
-        if (user == null) return null;
-        Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
-        grantedAuthorities.add(new SimpleGrantedAuthority(user.isAdmin() ? "ROLE_ADMIN" : "ROLE_USER"));
-        return new UsernamePasswordAuthenticationToken(email, passwordEncoder.encode(password), grantedAuthorities);
+        try {
+            final okhttp3.Response resp = Utils.authBlih(email, password);
+            JSONObject jsonObj = new JSONObject(resp.body().string());
+            Set<GrantedAuthority> grantedAuthorities = new HashSet<>();
+            final User user = userRepository.findByEmail(email);
+            if (user == null) {
+                User nUser = new User();
+                nUser.setEmail(email);
+                nUser.setAdmin(true);
+                userRepository.save(nUser);
+                grantedAuthorities.add(new SimpleGrantedAuthority(nUser.isAdmin() ? "ROLE_ADMIN" : "ROLE_USER"));
+                return new UsernamePasswordAuthenticationToken(email, passwordEncoder.encode(jsonObj.getString("token")), grantedAuthorities);
+            }
+            grantedAuthorities.add(new SimpleGrantedAuthority(user.isAdmin() ? "ROLE_ADMIN" : "ROLE_USER"));
+            return new UsernamePasswordAuthenticationToken(email, passwordEncoder.encode(jsonObj.getString("token")), grantedAuthorities);
+        } catch (NoSuchAlgorithmException e) {
+            e.printStackTrace();
+        } catch (SignatureException e) {
+            e.printStackTrace();
+        } catch (InvalidKeyException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return null;
     }
 
     @Override
