594192b4bfdb79abfab366b09ec20eceee73f202
Yann Carlen <yann-carlen@hotmail.fr>
2017-11-07 22:31:20 +0100

    Show projects list

--- src/main/java/com/epitools/homer/homer/controller/HomerController.java
+++ src/main/java/com/epitools/homer/homer/controller/HomerController.java
@@ -1,18 +1,39 @@
 package com.epitools.homer.homer.controller;
 
+import com.epitools.homer.homer.repository.ProjectRepository;
+import com.epitools.homer.homer.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 @Controller
 public class HomerController {
 
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @Autowired
+    UserRepository userRepository;
+
+    private void getProjects(final Map<String, Object> model) {
+        model.put("projects", projectRepository.findAll());
+        model.put("username",
+                userRepository.
+                        findByEmail(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString()));
+    }
+
     @GetMapping("/")
-    public String defaultPage() {
+    public String defaultPage(final Map<String, Object> model) {
+        getProjects(model);
         return "home";
     }
 
     @GetMapping("/home")
-    public String home() {
+    public String home(final Map<String, Object> model) {
+        getProjects(model);
         return "home";
     }
 
--- src/main/java/com/epitools/homer/homer/controller/ProjectController.java
+++ src/main/java/com/epitools/homer/homer/controller/ProjectController.java
@@ -1,10 +1,22 @@
 package com.epitools.homer.homer.controller;
 
+import com.epitools.homer.homer.repository.ProjectRepository;
+import com.epitools.homer.homer.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+import java.util.Map;
+
 @Controller
 public class ProjectController {
+
+    @Autowired
+    ProjectRepository projectRepository;
+
+    @Autowired
+    UserRepository userRepository;
 
     @GetMapping("/project/new")
     public String create() {
@@ -12,7 +24,11 @@ public class ProjectController {
     }
 
     @GetMapping("/project/all")
-    public String all() {
+    public String all(final Map<String, Object> model) {
+        model.put("projects", projectRepository.findAll());
+        model.put("username",
+                userRepository.
+                    findByEmail(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString()));
         return "project/all";
     }
 }
