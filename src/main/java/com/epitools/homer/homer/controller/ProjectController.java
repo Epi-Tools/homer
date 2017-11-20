0bd26e9fbab38d3b3a377e670a10da5ac1ef4057
Yann Carlen <yann-carlen@hotmail.fr>
2017-11-20 18:34:10 +0100

    display project by id

--- src/main/java/com/epitools/homer/homer/controller/ProjectController.java
+++ src/main/java/com/epitools/homer/homer/controller/ProjectController.java
@@ -1,11 +1,16 @@
 package com.epitools.homer.homer.controller;
 
+import com.epitools.homer.homer.model.Project;
 import com.epitools.homer.homer.repository.ProjectRepository;
 import com.epitools.homer.homer.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.MediaType;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 import java.util.Map;
 
@@ -30,5 +35,13 @@ public class ProjectController {
                 userRepository.
                     findByEmail(SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString()));
         return "project/all";
+    }
+
+    @RequestMapping(value="/project/{id}", method=RequestMethod.GET, produces={ MediaType.TEXT_HTML_VALUE })
+    public String byId(@PathVariable(value="id") final Integer projectId, final Map<String, Object> model) {
+        Project project = projectRepository.findOne(projectId);
+        if(project == null) model.put("error", "Wrong Project Id");
+        else model.put("project", project);
+        return "project/project";
     }
 }
