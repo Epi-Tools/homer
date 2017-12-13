82a191f04ec2bc0902b32648ff03fa90813c210e
Yann Carlen <yann-carlen@hotmail.fr>
2017-12-13 16:42:22 +0100

    fix delete project

--- src/main/java/com/epitools/homer/homer/rest/controller/ProjectRestController.java
+++ src/main/java/com/epitools/homer/homer/rest/controller/ProjectRestController.java
@@ -8,7 +8,6 @@ import com.epitools.homer.homer.util.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.security.access.annotation.Secured;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
@@ -76,8 +75,9 @@ public class ProjectRestController {
         return ResponseEntity.ok(updatedProject);
     }
 
+    // TODO: make check on project status
+    @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
     @RequestMapping(value="/projects/{id}", method=RequestMethod.DELETE, produces={ MediaType.APPLICATION_JSON_VALUE })
-    @Secured("ROLE_ADMIN")
     public ResponseEntity<Object> deleteProject(@PathVariable(value="id") Integer projectId) {
         final User maybeUser = Utils.getMaybeUser(userRepository);
         if (maybeUser == null) return Utils.jsonError("User not connected");
