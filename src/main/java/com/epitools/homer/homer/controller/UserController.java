6660af1d1b636688ae1659a0be12edaa1f44fb5e
Yann Carlen <yann-carlen@hotmail.fr>
2017-12-17 16:33:38 +0100

    users can know see their spices number in their profile page

--- src/main/java/com/epitools/homer/homer/controller/UserController.java
+++ src/main/java/com/epitools/homer/homer/controller/UserController.java
@@ -1,16 +1,26 @@
 package com.epitools.homer.homer.controller;
 
+import com.epitools.homer.homer.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.Map;
 
 @Controller
 @PreAuthorize("hasRole('USER') or hasRole('ADMIN')")
 public class UserController {
 
+    @Autowired
+    UserRepository userRepository;
+
     @GetMapping("/user")
-    public String user() {
-        return "user";
+    public String user(final Map<String, Object> model) {
+        final String user = SecurityContextHolder.getContext().getAuthentication().getPrincipal().toString();
+        model.put("user", userRepository.findByEmail(user));
+        return "user/user";
     }
 
 }
