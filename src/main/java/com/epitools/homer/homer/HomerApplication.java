6195425c2cdb42d08b78b4fb11d079321149b4a3
Yann Carlen <yann-carlen@hotmail.fr>
2018-03-05 01:27:13 +0100

    update with prod config

--- src/main/java/com/epitools/homer/homer/HomerApplication.java
+++ src/main/java/com/epitools/homer/homer/HomerApplication.java
@@ -2,9 +2,16 @@ package com.epitools.homer.homer;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.session.data.redis.config.ConfigureRedisAction;
 
 @SpringBootApplication
 public class HomerApplication {
+
+	@Bean
+	public static ConfigureRedisAction configureRedisAction() {
+		return ConfigureRedisAction.NO_OP;
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(HomerApplication.class, args);
--- src/main/java/com/epitools/homer/homer/security/SpringSecurityConfig.java
+++ src/main/java/com/epitools/homer/homer/security/SpringSecurityConfig.java
@@ -2,6 +2,7 @@ package com.epitools.homer.homer.security;
 
 import com.epitools.homer.homer.provider.CustomAuthenticationProvider;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
@@ -26,34 +27,27 @@ public class SpringSecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     CustomAuthenticationHandler customAuthenticationHandler;
 
-    // roles admin allow to access /admin/**
-    // roles user allow to access /user/**
-    // custom 403 access denied handler
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.csrf().disable()
-            .authorizeRequests()
-            .antMatchers("/","/public/**", "/resources/**",
-                    "/resources/public/**", "/css/**", "/js/**", "/webjars/**").permitAll()
-            .antMatchers("/", "/home", "/about").permitAll()
-            // .antMatchers("admin/**", "api/**", "project/**").hasRole("ADMIN")
-            // .antMatchers("/user/**", "project/**", "api/projects/**").hasRole("USER")
-            .anyRequest().authenticated()
-            .and()
-            .formLogin()
-            .loginPage("/login")
-            .defaultSuccessUrl("/", true)
-            .failureUrl("/login?error")
-            .failureHandler(customAuthenticationHandler)
-            .permitAll()
-            .and()
-            .logout()
-            .permitAll()
-            .and()
-            .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
+                .authorizeRequests()
+                .antMatchers("/","/public/**", "/resources/**",
+                        "/resources/public/**", "/css/**", "/js/**", "/webjars/**").permitAll()
+                .antMatchers("/", "/home", "/about").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .defaultSuccessUrl("/", true)
+                .failureHandler(customAuthenticationHandler)
+                .permitAll()
+                .and()
+                .logout()
+                .permitAll()
+                .and()
+                .exceptionHandling().accessDeniedHandler(accessDeniedHandler);
     }
 
-    // create two users, admin and user
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.authenticationProvider(authProvider);
